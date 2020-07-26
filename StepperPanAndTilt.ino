3c0ed6a40604e143bde6dd5c9a8870c1f1215f71
james wolf <contractorwolf@hotmail.com>
2020-07-26 15:10:32 -0400

    pan and tilt working, no longer hitting breakpoints

--- StepperPanAndTilt.ino
+++ StepperPanAndTilt.ino
@@ -20,8 +20,6 @@ int lastRead = 0;  // variable to store the value read
 
 
 int fullSpin = 1600;// 1600 is 360 exactly
-int chopDelay = 400;//200
-int chopSplit = 400;//150
 
 
 int offset = 30;
@@ -52,8 +50,8 @@ int last9UD = 0;
 int last10UD = 0;
 int last11UD = 0;
 
-int currentPositionLR = 0;
-int currentPositionUD = 0;
+volatile int currentPositionLR = 0;
+volatile int currentPositionUD = 0;
 
 volatile int measureLeft = 0;
 volatile int measureRight = 0;
@@ -62,12 +60,22 @@ volatile int measureUp = 0;
 volatile int measureDown = 0;
 
 
+volatile int directionLR = 0; //left
+volatile int directionUD = 0; //up
+
 
 byte calibrated = false;
 byte leftRight = false;
 
 int lastAvgLR = 0;
 int lastAvgUD = 0;
+
+int chopDelayPan = 1500;//200
+int chopSplitPan = 1500;//150
+int chopDelay = 400;//200
+int chopSplit = 400;//150
+
+
 
 static unsigned long last;
 
@@ -103,130 +111,164 @@ void loop() {
 
     if(buttonStatus){
       Serial.println("button pressed");
+      calibrated = false;
 
-      //measure LR side to side 
-      stopStateLR = false;      
-      measureRight = measureLR(fullSpin, 1, STEPDIRECTIONLR, STEPPINLR);
-      moveLR(5, chopSplit, chopDelay, 0);
-      
-      delay(10);   
-      stopStateLR = false;
-      measureLeft = measureLR(fullSpin, 0, STEPDIRECTIONLR, STEPPINLR);
-      
-      delay(10); 
+      calibrated = calibrate();
 
-      centerLR  = (measureLeft/2) + offset;
-      stopStateLR = false;
-      moveLR(centerLR, chopSplit, chopDelay, 1);
-      currentPositionLR = centerLR;
-
-      //measure UD top to bottom
-      stopStateUD = false;      
-      measureUp = measureUD(fullSpin, 1, STEPDIRECTIONUD, STEPPINUD);
-      moveUD(5, chopSplit, chopDelay, 0);
-      
-      delay(10);   
-      stopStateUD = false;
-      measureDown = measureUD(fullSpin, 0, STEPDIRECTIONUD, STEPPINUD);
-      
-      delay(10); 
-
-      centerUD  = (measureDown/2) + 0; //offset
-      stopStateUD = false;
-      moveUD(centerUD, chopSplit, chopDelay, 1);
-      currentPositionUD = centerUD;
-
-      calibrated = true;
     }else{
       int currentReadLR = analogRead(analogPinLR);
       int currentReadUD = analogRead(analogPinUD);
       
       int avgLR = averageLastLR(currentReadLR);
-      int mapLR = map(avgLR, 0, 1024, 906, 0);
+      int mapLR = map(avgLR, 0, 1024, 850, 150);
       int diffLR = currentPositionLR - mapLR;    
 
 
       int avgUD = averageLastUD(currentReadUD);
-      int mapUD = map(avgUD, 0, 1024, 500, 0);
+      int mapUD = map(avgUD, 0, 1024, 350, 30);
       int diffUD = currentPositionUD - mapUD;    
 
-       
-//      
-//      Serial.print(" currentPositionLR: ");
-//      Serial.print(currentPositionLR);
-//
-//      Serial.print(" currentReadLR: ");
-//      Serial.print(currentReadLR);
-//      
-//      Serial.print(" currentReadUD: ");
-//      Serial.print(currentReadUD);
-//
-//      Serial.print(" avgLR: ");
-//      Serial.print(avgLR);
-//      
-//      Serial.print(" mapLR: ");
-//      Serial.print(mapLR);
-//
-//      Serial.print(" diffLR: ");
-//      Serial.print(diffLR);
-//      
-//      Serial.print(" diffUD: ");
-//      Serial.print(diffUD);
-
-    
       int mvAmountLR = 0;
       int mvAmountUD = 0;
       
       int minChngLR = 50;
       int minChngUD = 30;
 
-      
-      int chopDelayPan = 1500;//200
-      int chopSplitPan = 1500;//150
-      
       if(calibrated){
         // left right movement
         if(diffLR > 0 && abs(diffLR)>minChngLR){
-          mvAmountLR = moveLR(diffLR, chopSplitPan, chopDelayPan, 1);
+          mvAmountLR = moveLR(diffLR, 1);
           currentPositionLR = currentPositionLR - mvAmountLR;
+          Serial.print(" newCurrentLR: ");      
+          Serial.println(currentPositionLR);
         }else if(diffLR < 0 && abs(diffLR)>minChngLR){
-          mvAmountLR = moveLR(abs(diffLR), chopSplitPan, chopDelayPan, 0);        
+          mvAmountLR = moveLR(abs(diffLR), 0);        
           currentPositionLR = currentPositionLR + mvAmountLR;
+          Serial.print(" newCurrentLR: ");      
+          Serial.println(currentPositionLR);
         }
 
         // updown stepper movement
         if(diffUD > 0 && abs(diffUD)>minChngUD){
-          Serial.print("MOVE UD-----------------------------------");        
-          mvAmountUD = moveUD(diffUD, chopSplitPan, chopDelayPan, 1);
+          //Serial.print("MOVE UD-----------------------------------");      
+          mvAmountUD = moveUD(diffUD, 1);
           currentPositionUD = currentPositionUD - mvAmountUD;
+          Serial.print(" newCurrentUD: ");      
+          Serial.println(currentPositionUD);
         }else if(diffUD < 0 && abs(diffUD)>minChngUD){
-          Serial.print("MOVE UD------------------------------------");    
-          mvAmountUD = moveUD(abs(diffUD), chopSplitPan, chopDelayPan, 0);        
+          //Serial.print("MOVE UD------------------------------------");    
+          mvAmountUD = moveUD(abs(diffUD), 0);        
           currentPositionUD = currentPositionUD + mvAmountUD;
+          Serial.print(" newCurrentUD: ");      
+          Serial.println(currentPositionUD);
         }
       }
-
-      Serial.print(" newCurrentLR: ");
-      Serial.print(currentPositionLR);
-      Serial.print(" newCurrentUD: ");
-      Serial.println(currentPositionUD);
+//
+//      Serial.print(" newCurrentLR: ");
+//      Serial.print(currentPositionLR);
+//      Serial.print(" newCurrentUD: ");      
+//      Serial.println(currentPositionUD);
 
 
     }
 }
 
+bool calibrate(){
+
+  calibrateLR();
+  
+  calibrateUD();
+  
+  return true;
+}
+
+
+void calibrateLR(){
+  //measure LR side to side 
+  stopStateLR = false;      
+  measureRight = measureLR(fullSpin, 1, STEPDIRECTIONLR, STEPPINLR);
+  moveLR(5, 0);
+  
+  delay(10);   
+  stopStateLR = false;
+  measureLeft = measureLR(fullSpin, 0, STEPDIRECTIONLR, STEPPINLR);
+  
+  delay(10); 
+  
+  centerLR  = (measureLeft/2) + offset;
+  stopStateLR = false;
+  moveLR(centerLR, 1);
+  currentPositionLR = centerLR;
+}
+
+void calibrateUD(){
+    //measure UD top to bottom
+  stopStateUD = false;      
+  measureUp = measureUD(fullSpin, 1, STEPDIRECTIONUD, STEPPINUD);
+  moveUD(5, 0);
+  
+  delay(10);   
+  stopStateUD = false;
+  measureDown = measureUD(fullSpin, 0, STEPDIRECTIONUD, STEPPINUD);
+  
+  delay(10); 
+  
+  centerUD  = (measureDown/2) + 0; //offset
+  stopStateUD = false;
+  moveUD(centerUD, 1);
+  currentPositionUD = centerUD;
+}
+
+
+
 void stopLR(){
   stopStateLR = true;
+
+  // look at current direction L or R
+  // if L then set current position to max left
+  // if R then set current poistion to max right 
+  
   Serial.println("stop LR");
 }
 void stopUD(){
   stopStateUD = true;
+
   Serial.println("stop UD");
+  Serial.print("directionUD: ");
+  Serial.print(directionUD);
+  Serial.print(" currentPositionUD");
+  Serial.println(currentPositionUD);
+
+  
+
+//// 1 is down, 0 is up
+//  if(directionUD == 1){
+//    moveUD(5, 1);
+//    currentPositionUD = 0;
+//  }else if(directionUD == 0){
+//    moveUD(5, 0);
+//    currentPositionUD = 500;
+//  }
+//
+
+
+//  Serial.print(" new currentPositionUD");
+//  Serial.println(currentPositionUD);
+//
+//  // look at current direction U or D
+//  // if U then set current position to max up
+//  // if D then set current poistion to max down 
+//  Serial.println("stop UD");
 }
 
 int measureLR(int times, int dir, int dirPin, int stepPin){
   int index = 0;
 
+//  Serial.print("direction: ");// 1 is right, 0 is left
+//  Serial.println(dir);
+
+  directionLR = dir;
+  
   if(dir){
     digitalWrite(dirPin, HIGH);
   }else{
@@ -246,6 +288,13 @@ int measureLR(int times, int dir, int dirPin, int stepPin){
 int measureUD(int times, int dir, int dirPin, int stepPin){
   int index = 0;
 
+//  
+//  Serial.print("direction: ");// 1 is down, 0 is up
+//  Serial.println(dir);
+
+  directionUD = dir;
+
+
   if(dir){
     digitalWrite(dirPin, HIGH);
   }else{
@@ -264,26 +313,29 @@ int measureUD(int times, int dir, int dirPin, int stepPin){
 
 // need to combine moveUD and moveLR into a single method that looks at the difference between the current position and suggested position for each 
 // stepper and moves in the suggested direction to achieve that position on each iteration in order to not waste and cycles
-int moveUD(int times, int flash_delay, int off_delay, int dir){
+int moveUD(int times, int dir){
+  stopStateUD = false;
+  
   int index = 0;
   if(dir){
     digitalWrite(STEPDIRECTIONUD, HIGH);
   }else{
     digitalWrite(STEPDIRECTIONUD, LOW);
   }
-  
-  while(index<times){
+
+  while(index<times && stopStateUD == false){
     digitalWrite(STEPPINUD, LOW);
-    delayMicroseconds(flash_delay);
+    delayMicroseconds(chopSplitPan);
     digitalWrite(STEPPINUD, HIGH);
-    delayMicroseconds(off_delay);
+    delayMicroseconds(chopDelayPan);
     index = index + 1;
   }
   return index;
 }
 
-int moveLR(int times, int flash_delay, int off_delay, int dir){
+int moveLR(int times, int dir){
   int index = 0;
+
   if(dir){
     digitalWrite(STEPDIRECTIONLR, HIGH);
   }else{
@@ -292,16 +344,16 @@ int moveLR(int times, int flash_delay, int off_delay, int dir){
   
   while(index<times){
     digitalWrite(STEPPINLR, LOW);
-    delayMicroseconds(flash_delay);
+    delayMicroseconds(chopSplitPan);
     digitalWrite(STEPPINLR, HIGH);
-    delayMicroseconds(off_delay);
+    delayMicroseconds(chopDelayPan);
     index = index + 1;
   }
   return index;
 }
 
 
-
+//******************************************************************************************************
 
 int averageLastLR(int incoming){
   if(last10LR == 0){last10LR = incoming;}
